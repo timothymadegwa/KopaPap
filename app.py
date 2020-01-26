df18ee3b554aa7f99563fa23bcd992f965a39b6e
Timothy Madegwa <timothymadegwa@gmail.com>
2020-01-26 15:28:08 +0300

    making inputs more specific

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def predict():
     return render_template('index.html', prediction_text='Probability of repayment is: {} %'.format(output))
 
 
-#if __name__ == "__main__":
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
