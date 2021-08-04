819e8d97354011a947f3708847884d6c4a58cb9d
Timothy Madegwa <timothymadegwa@gmail.com>
2021-08-04 22:07:52 +0300

    added comments

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
+# importing relevant libraries
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
 
+# create an instance of a flask app
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-kes_to_usd = 100
-DEBUG = False
 
+# load our pre-trained model
+model = pickle.load(open('model.pkl', 'rb'))
+
+# hypothetical currency conversion
+kes_to_usd = 100
+
+# Creating a route for the home page
 @app.route('/')
 def home():
     return render_template("index.html")
 
-
+# creating a route for the predict page
 @app.route('/predict',methods=['POST', 'GET'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
+    # extraction of the form values
     features = [*request.form.values()]
-    
+
+    # extract the amount 
     amt = features[8]
     
     try:
+        # converting all values in the list to floats
         features = [*map(float,features)]
     except ValueError:
+        # incase of a value error, it redirects to the home page
         return render_template('index.html', error=1, warning_text ="kindly enter a integer values in the relevant fields")
+    
+    # calculating the loan to income ratio
     features.append((features[6]*12)/features[8])
     
+    # converting the list to a numpy array
     features = np.array([features])
+
+    # Converting values to kes
     features[0][6] = (features[0][6]*12)/kes_to_usd
     features[0][7] = (features[0][7]*12)/kes_to_usd
     features[0][8] = features[0][8]/kes_to_usd
 
+    # using the pre-trained model to make predictions
     prediction = model.predict_proba(features)
         
-
+    # converting to a % age and rounding off to 2 dp
     output = round((prediction[0][1])*100,2)
 
     return render_template('report.html', text='The probability of repayment for on a loan of KES {} by the Client is: {} %'.format(amt, output))
--- model.py
+++ model.py
@@ -1,24 +1,33 @@
-import numpy as np
+# importing relevant libraries
 import pandas as pd
 import pickle
 from sklearn.linear_model import LogisticRegression
 
+# reading/loading in the training data
 data = pd.read_csv('data/train.csv')
 
+# replacing missing data with the modal value
 data.Credit_History.fillna(data.Credit_History.mode()[0],inplace=True)
 data.Gender.fillna(data.Gender.mode()[0],inplace=True)
 data.Married.fillna(data.Married.mode()[0],inplace=True)
 data.Dependents.fillna(data.Dependents.mode()[0],inplace=True) 
 data.Self_Employed.fillna(data.Self_Employed.mode()[0],inplace=True) 
+
+# changing the format for '3+' dependants
 data.Dependents = data.Dependents.replace('3+','3')
+
+# filling other missing data with the mean
 data = data.fillna(data.mean())
 
+# creating mappers for the different columns
 gender_mapper = {'Male':1, 'Female':0}
 married_mapper = {'Yes':1, 'No':0}
 educ_mapper = {'Graduate':1, 'Not Graduate':0}
 self_emp_mapper = {'Yes':1, 'No':0}
 prop_area_mapper = {'Semiurban':0, 'Urban':1, 'Rural':2}
 loan_mapper = {'Y':1, 'N':0}
+
+# mapping the data to categorical values
 data.Gender = data.Gender.map(gender_mapper)
 data.Married = data.Married.map(married_mapper)
 data.Education = data.Education.map(educ_mapper)
@@ -26,20 +35,31 @@ data.Self_Employed = data.Self_Employed.map(self_emp_mapper)
 data.Property_Area = data.Property_Area.map(prop_area_mapper)
 data.Loan_Status = data.Loan_Status.map(loan_mapper)
 
+# converting dependants to interger values
 data.Dependents = data.Dependents.astype(int)
+
+# creating an income to loan ratio
 data['income_to_loan'] = data['ApplicantIncome'] / data['LoanAmount']
 
+# dropping the loan ID column
 data.drop('Loan_ID', axis=1, inplace=True)
-#data = pd.get_dummies(data, drop_first=True)
 
+# isolating the loan status (target) values
 target = data.Loan_Status
+
+# dropping the loan status from original dataset
 features = data.drop('Loan_Status',axis=1)
 
+# creating the features to be used for training
 features = data[['Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History', 
                 'Property_Area','ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
                 'Loan_Amount_Term', 'income_to_loan', 'Gender']]
 
+# Creating an instance of logistic regression
 lr = LogisticRegression(C= 0.1, solver = 'liblinear', warm_start = True)
+
+# training the model using logistic regression
 lr.fit(features, target)
 
+# saving the model as a pickle file
 pickle.dump(lr, open('model.pkl','wb'))
